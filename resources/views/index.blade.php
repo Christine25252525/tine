48f855e82b34bab5d4bda07746aa102bc068a274
christine alcaide <alcaidecj.782.stud@cdd.edu.ph>
2024-07-18 16:40:26 +0800

    laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Christine Joy Alcaide</title>
+</head>
+<body>
+<h1>ITP17 ADVANCED PROGRAMMING</h1>
+
+<a href="{{ ur1('/posts') }}">POST</a>
+</body>
+</html>
